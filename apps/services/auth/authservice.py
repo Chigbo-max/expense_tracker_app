394e2ac31d130222923ef8f798889576be6a5a06
Chigbo Ezeokeke <ezeokeke.chigbo@gmail.com>
2025-04-03 14:10:26 +0100

    updated the authentications with tests

--- apps/services/auth/authservice.py
+++ apps/services/auth/authservice.py
@@ -1,8 +1,10 @@
+import traceback
 from datetime import timedelta
 
 from flask import jsonify
 from flask_jwt_extended import create_access_token, create_refresh_token
 
+from apps.data.model.status import AccountStatus
 from apps.data.model.user import User
 from apps.services.auth.authInterface import AuthInterface
 
@@ -12,20 +14,21 @@ class AuthService(AuthInterface):
     def register(self, data):
         try:
             if User.objects(email=data['email']).first():
-                return jsonify({f"status": "error", "message": "Account already registered"}), 400
+                return jsonify({f"status": "error", "message": "This account is already registered"}), 400
 
             email = data['email'].strip().lower()
 
             existing_admin = User.objects(role="admin").first()
             role = "admin" if not existing_admin else "user"
 
-            user = User(email=email, role=role)
+            user = User(email=email, role=role, status = AccountStatus.ACTIVE)
             user.save()
 
             access_token = create_access_token(identity=email, expires_delta=timedelta(minutes=40))
             refresh_token = create_refresh_token(identity=email, expires_delta=timedelta(days=2))
             return jsonify({f"status": "success",
-                            "message": "User registered",
+                            "message": f"{email} registered successfully",
+                            "role": role,
                             "access_token": access_token,
                             "refresh_token": refresh_token}), 200
 
@@ -33,4 +36,29 @@ class AuthService(AuthInterface):
             return jsonify({f"status": "error", "message": str(e)}), 500
 
     def login(self, data):
-        pass
+
+        try:
+
+            email = data['email'].strip().lower()
+
+            user = User.objects(email=email).first()
+
+            if not user:
+                return jsonify({f"status": "error", "message": "This account is not registered"}), 400
+
+            if user.status != AccountStatus.ACTIVE:
+                return jsonify({f"status": "error", "message": "This account is not active please contact support"}), 400
+
+
+            access_token = create_access_token(identity=email, expires_delta=timedelta(minutes=40))
+            refresh_token = create_refresh_token(identity=email, expires_delta=timedelta(days=2))
+
+            return jsonify({f"status": "success",
+                            "message": f"{email} login successful",
+                            "access_token": access_token,
+                            "refresh_token": refresh_token
+                            }), 200
+
+        except Exception as e:
+            print(traceback.format_exc())
+            return jsonify({f"status": "error", "message": str(e)}), 500
---
+++ test/test_authservice.py
@@ -0,0 +1,91 @@
+from unittest import TestCase
+
+import mongomock
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from mongoengine import connect
+
+from apps.data.model.user import User
+from apps.services.auth.authservice import AuthService
+
+
+class TestAuthService(TestCase):
+
+
+    @classmethod
+    def setUpClass(cls):
+        cls.app = Flask(__name__)
+        cls.app.config["TESTING"] = True
+        cls.app.config["JWT_SECRET_KEY"] = "test_secret"
+        cls.jwt = JWTManager(cls.app)
+        connect('mongoenginetest', host='mongodb://localhost', mongo_client_class=mongomock.MongoClient)
+
+
+    def setUp(self):
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        User.objects.delete()
+
+    def tearDown(self):
+        self.app_context.pop()
+
+    def test_that_registration_works_return_count_of_one(self):
+        auth_service = AuthService()
+        data = {"email": "test_email@gmail.com"}
+        response, status = auth_service.register(data)
+        response_json = response.get_json()
+
+        assert status == 200
+        assert response_json['status'] == 'success'
+        assert response_json['message'] == 'test_email@gmail.com registered successfully'
+        assert User.objects.count() == 1
+
+    def test_that_an_already_registered_account_cannot_register_twice(self):
+        auth_service = AuthService()
+        data = {"email": "test_email@gmail.com"}
+        response, status = auth_service.register(data)
+        response_json = response.get_json()
+
+        assert status == 200
+        assert response_json['status'] == 'success'
+        assert response_json['message'] == 'test_email@gmail.com registered successfully'
+
+        data2 = {"email": "test_email@gmail.com"}
+        response, status = auth_service.register(data2)
+        response_json = response.get_json()
+
+        assert status == 400
+        assert response_json['status'] == 'error'
+        assert response_json['message'] == "This account is already registered"
+
+
+    def test_that_login_function_status_returns_200(self):
+        auth_service = AuthService()
+        data = {"email": "test_email@gmail.com"}
+        response, status = auth_service.register(data)
+        response_json = response.get_json()
+
+        assert status == 200
+        assert response_json['status'] == 'success'
+        assert response_json['message'] == 'test_email@gmail.com registered successfully'
+
+        data2 = {"email": "test_email@gmail.com"}
+        response, status = auth_service.register(data2)
+        response_json = response.get_json()
+
+        assert status == 400
+        assert response_json['status'] == 'error'
+        assert response_json['message'] == "This account is already registered"
+
+
+        auth_service = AuthService()
+        data = {"email": "test_email@gmail.com"}
+        response, status = auth_service.login(data)
+        response_json = response.get_json()
+
+        assert status == 200
+        assert response_json['status'] == 'success'
+        assert response_json['message'] == 'test_email@gmail.com login successful'
+
+
+
